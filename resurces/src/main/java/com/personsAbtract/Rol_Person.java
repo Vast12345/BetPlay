d78b45a1693353a88eda766a001d25cfff4f4f44
juandavidg0mez <nihlgomez@gmail.com>
2024-06-30 16:36:36 -0500

    wow

--- resurces/src/main/java/com/personsAbtract/Rol_Person.java
+++ resurces/src/main/java/com/personsAbtract/Rol_Person.java
@@ -1,5 +1,18 @@
 package com.personsAbtract;
 
 public abstract class Rol_Person {
+    protected String nombreRol;
 
+    protected Rol_Person(){
+    }
+    
+    protected Rol_Person(String nombreRol){
+        this.nombreRol = nombreRol;
+    }
+
+    public String getNombreRol(){
+        return nombreRol;
+    }
+
+    public abstract String decirRol();
 }
--- resurces/src/main/java/com/personsAbtract/person.java
+++ resurces/src/main/java/com/personsAbtract/person.java
@@ -1,5 +1,71 @@
 package com.personsAbtract;
 
-public class person {
+public class person{
+    private String Nombre;
+    private String Apellido;
+    private String dni;
+    private String edad;
+    private String contacto;
+    private Rol_Person nombreRol;
 
+    public person(String Apellido, String Nombre, String contacto, String dni, String edad, Rol_Person nombreRol) {
+        this.Apellido = Apellido;
+        this.Nombre = Nombre;
+        this.contacto = contacto;
+        this.dni = dni;
+        this.edad = edad;
+        this.nombreRol = nombreRol;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public Rol_Person getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(Rol_Person nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+    
+    
+
+    
 }
